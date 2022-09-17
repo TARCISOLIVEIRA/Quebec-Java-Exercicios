de05132002fca9bffbfb3a71180ee7ed755ce491
Tarciso dos Santos de Oliveira <84403833+TARCISOLIVEIRA@users.noreply.github.com>
2022-09-17 13:56:33 -0300

    atualiza

---
+++ caixaeletronico/ caixaeletronico/src/teste.java
@@ -0,0 +1,11 @@
+public class teste {
+    public static void main(String[] args) {
+        double saldo = 25;
+        double valorSolicitado = 17;
+        if(valorSolicitado < saldo)
+        saldo = saldo - valorSolicitado;
+        System.out.println(saldo);
+        
+    }
+    
+}
